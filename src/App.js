5cdd1781491de4cd329929068e8309f6dbec8ee2
tusharjaiswal2k <tusharjaiswal2k>
2021-02-10 13:37:03 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import { ToDoList } from "./component/to-do-list/to-do-list";
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      <ToDoList></ToDoList>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/to-do-list/to-do-list.js
@@ -0,0 +1,84 @@
+import React, { createRef } from "react";
+
+
+export class ToDoList extends React.Component{
+
+    itemList = [];
+    inputRef = React.createRef();
+    getLocalStorage;
+    isButtonActive = false;
+
+    constructor(){  
+        super();
+    }
+
+    componentDidMount() {
+        this.itemList = JSON.parse(localStorage.getItem("New Todo"));
+        this.setState(this.itemList);
+        this.onInputChange(this.inputRef);
+    }
+    onInputChange(inputRef){
+        if(inputRef.current.value.length > 0){
+            this.isButtonActive = true;
+        }
+        else{
+            this.isButtonActive = false;
+        }
+
+        this.setState({
+            isButtonActive: this.isButtonActive
+        });
+        
+    }
+    showTasks(){
+        this.itemList =JSON.parse(this.getLocalStorage);
+        this.setState(this.itemList);
+    };
+    
+    pushItemInList(inputRef){
+        this.isButtonActive = false;
+        this.setState({
+            isButtonActive: this.isButtonActive
+        })
+        this.itemList.push(inputRef.current.value);
+        localStorage.setItem("New Todo",JSON.stringify(this.itemList)); //transforming js object into a json string
+        this.setState(this.itemList);
+        inputRef.current.value = "";
+    }    
+    deleteTask(index){        
+        this.itemList.splice(index,1);
+        localStorage.setItem("New Todo",JSON.stringify(this.itemList)); //transforming js object into a json string
+        this.setState(this.itemList);
+    }
+    deleteAllTasks(){
+        this.itemList = [];
+        localStorage.setItem("New Todo",JSON.stringify(this.itemList)); //transforming js object into a json string
+        this.setState(this.itemList);   
+    }
+    
+    render(){
+        return(
+            <div>
+                <div className="wrapper">
+                    <header>Todo App</header>
+                    <div className="inputField">
+                        <input type="text" ref={this.inputRef} placeholder="Add your new todo" onChange={() => { this.onInputChange(this.inputRef)}} />
+                        <button onClick={this.pushItemInList.bind(this,this.inputRef)} className={`${this.isButtonActive ? 'active':'null'}`}><i className="fas fa-plus"></i></button>
+                    </div>
+                    <ul className="todoList">
+                        {this.itemList.map((item,index) => {
+                            return (                     
+                                <li key={index}>{item}<span onClick ={()=>this.deleteTask(index)}><i className="fas fa-trash"></i></span></li>
+                                )
+                            })} 
+                    </ul>
+                    <div className="footer">
+                        <span>you have {this.itemList.length} pending task</span>
+                        <button className={`${this.itemList.length == 0 ? 'null' : 'active'}`} onClick ={()=>this.deleteAllTasks()} >clear all</button>
+                        
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
